cc887fc5a23361b2e1ad49a71756f721f62d9242
remco <remcoad@gmail.com>
2020-12-07 00:05:55 +0100

    Added Synerbay plugin

--- plugins/synerbay/synerbay.php
+++ plugins/synerbay/synerbay.php
@@ -1,82 +1,54 @@
 <?php
-
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              remcofy.nl
- * @since             1.0.0
- * @package           Synerbay
- *
- * @wordpress-plugin
- * Plugin Name:       Synerbay
- * Plugin URI:        Synerbay
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Synerbay
- * Author URI:        remcofy.nl
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       synerbay
- * Domain Path:       /languages
+ * Plugin Name: SynerBay
+ * Description: Offer apply, invite, RFQ
+ * Version: 1.0
+ * Author: Kristóf Nagy
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+namespace SynerBay;
+
+if( ! defined( 'ABSPATH' ) ) {
+    return;
+}
+
+class SynerBay {
+    /**
+     * Loading all dependencies
+     * @return void
+     */
+    public function load() {
+        add_action( 'wp_enqueue_scripts', [$this, 'loadScript'], 10);
+        // basic files
+        include_once 'WPActionLoader.php';
+        include_once 'HTMLElement.php';
+        include_once 'Ajax.php';
+        include_once 'initPlugin.php';
+    }
+
+    public function loadScript() {
+        // load synerbay.js
+        wp_register_script( "synerbay-script", get_stylesheet_directory_uri() . '/synerbay.js');
+
+        // load synerbay.js
+        wp_enqueue_script("synerbay-script", get_stylesheet_directory_uri() . '/synerbay.js', ['jquery'], false, true);
+
+        // localize the script to your domain name, so that you can reference the url to admin-ajax.php file easily
+        wp_localize_script( 'synerbay-script', 'synerbayAjax', array(
+            'restURL' => rest_url(),
+            'restNonce' => wp_create_nonce('wp_rest')
+        ));
+    }
 }
 
 /**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
+ * Loading my plugin
+ * @return void
  */
-define( 'SYNERBAY_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-synerbay-activator.php
- */
-function activate_synerbay() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-synerbay-activator.php';
-	Synerbay_Activator::activate();
+function synerBayLoad() {
+    $pg = new SynerBay();
+    $pg->load();
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-synerbay-deactivator.php
- */
-function deactivate_synerbay() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-synerbay-deactivator.php';
-	Synerbay_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_synerbay' );
-register_deactivation_hook( __FILE__, 'deactivate_synerbay' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-synerbay.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_synerbay() {
-
-	$plugin = new Synerbay();
-	$plugin->run();
-
-}
-run_synerbay();
+// We need to call the function with the namespace
+add_action('plugins_loaded', 'SynerBay\synerBayLoad');
